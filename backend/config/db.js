e0c9ddc1b814df335576c4a7df45fd246b7fe84d
ShivangNagta <shivangnag@gmail.com>
2025-01-22 22:32:06 +0530

    mongo atlas setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,18 @@
+require("dotenv").config(); // Load environment variables
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/freelancingWebsite", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const MONGO_URI = process.env.MONGO_URI;
+    console.log(MONGO_URI)
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in the .env file.");
+    }
+
+    await mongoose.connect(MONGO_URI);
     console.log("Database connected successfully!");
   } catch (error) {
-    console.error("Error connecting to database", error);
+    console.error("Error connecting to database:", error.message);
     process.exit(1);
   }
 };
